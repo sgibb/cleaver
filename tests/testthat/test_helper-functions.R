308652dd50b6776820ef9eb5d1fe3cd6864d5a8a
s.gibb <unknown>
2014-05-03 15:03:03 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 13 commit(s).
    Commit information:
    
        Commit id: 05229428d0680a8458b8c34a4e7f22cf416781f6
        Commit message:
        fix return value of cleavageSites,AAStringSet
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-02T21:37:19+01:00
    
        Commit id: 1bdf58e738209075fc611a39c5ebd23040c4bf68
        Commit message:
        new vectorized split pos calculation
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T00:36:17+01:00
    
        Commit id: abcf853168167d48225fd0d9e833b182e86ff273
        Commit message:
        cleave,AAString returns an AAStringSet instead of an AAStringSetList instance
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T00:41:41+01:00
    
        Commit id: bab5ef9e638308f8a719fd4c38efc35fee662c3e
        Commit message:
        add column names
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T01:07:55+01:00
    
        Commit id: 51836374f6ba344d06c66d13f7b731128aa156c1
        Commit message:
        add list names
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T01:08:31+01:00
    
        Commit id: 5908b9786a9f5f4165d83be5c89b1bae1f91d7f0
        Commit message:
        update documentation
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T01:08:37+01:00
    
        Commit id: 2c94f3db8dec6815fb60c78f5fa0d90a08dc3dd5
        Commit message:
        add cleavageRanges for character,AAString and AAStringSet
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T02:18:22+01:00
    
        Commit id: 1e622fa047349603ec028a5e72edcf0f35446efb
        Commit message:
        add overview table to manual page
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T12:59:43+01:00
    
        Commit id: 7c8e1d91c8a54e8ac6722c7426bec796b7fdf113
        Commit message:
        more unit tests
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T15:32:56+01:00
    
        Commit id: 783972a0218b117ff340d95b033ee7d458f02033
        Commit message:
        update vignette
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T15:38:45+01:00
    
        Commit id: 513f8f66124c4d150e578c687f68a0404a48938a
        Commit message:
        bump date
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T15:39:25+01:00
    
        Commit id: 17e45a1952a86f02de29205bb8702dc1a73614cb
        Commit message:
        Merge branch 'master' of github.com:sgibb/cleaver
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T15:40:34+01:00
    
        Commit id: 01bed0ab2124198781e9c1c78ea5b3f9a5fe6d9c
        Commit message:
        Merge branch 'IRanges'
        Committed by Sebastian Gibb <mail at sebastiangibb.de>
        Commit date: 2014-05-03T16:02:38+01:00
    
    From: Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cleaver@89884 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_helper-functions.R
+++ tests/testthat/test_helper-functions.R
@@ -5,7 +5,31 @@ test_that(".rxPos", {
                list(integer(), 7))
 })
 
-test_that(".substring", {
-  expect_equal(cleaver:::.substring("foobar", c(3, 5)), c("foo", "ba", "r"))
+test_that(".pos", {
+  m0 <- matrix(c(1, 3, 6, 8, 2, 5, 7, 10), ncol=2L,
+               dimnames=list(c(), c("start", "end")))
+  m2 <- matrix(c(1, 3, 7, 10), ncol=2L,
+               dimnames=list(c(), c("start", "end")))
+  m02 <- matrix(c(1, 3, 6, 8, 1, 3, 6, 1, 3, 2, 5, 7, 10, 5, 7, 10, 7, 10),
+                ncol=2L, dimnames=list(c(), c("start", "end")))
+  expect_equal(cleaver:::.pos(c(2, 5, 7), 10, 0), m0)
+  expect_equal(cleaver:::.pos(c(2, 5, 7), 10, 2), m2)
+  expect_equal(cleaver:::.pos(c(2, 5, 7), 10, 0:2), m02)
+})
+
+test_that(".sequence", {
+  expect_identical(cleaver:::.sequence(1:5), sequence(1:5))
+})
+
+test_that(".revsequence", {
+  expect_identical(cleaver:::.revsequence(1:5), c(5, 4:5, 3:5, 2:5, 1:5))
+})
+
+test_that(".unlist", {
+  l <- list(a=1:3, b=1:5, c=LETTERS[1:7])
+  nl <- list(a=list(d=1:3, e=LETTERS[1:2]), b=1:5, c=LETTERS[1:7])
+  expect_identical(unname(unlist(l)), cleaver:::.unlist(l))
+  expect_identical(unlist(nl, use.names=FALSE, recursive=FALSE),
+                   cleaver:::.unlist(nl))
 })
 
