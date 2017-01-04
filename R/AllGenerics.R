7c626f6ba3eebe8db0c3ac3da3b560ab332e9d64
s.gibb <unknown>
2017-01-04 12:16:56 +0000

    remove GPL header
    From: Sebastian Gibb <mail@sebastiangibb.de>
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cleaver@125631 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,18 +1,3 @@
-## Copyright 2013 Sebastian Gibb
-## <mail@sebastiangibb.de>
-##
-## This is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## It is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## See <http://www.gnu.org/licenses/>
-
 if (is.null(getGeneric("cleave"))) {
   setGeneric("cleave", function(x, ...)standardGeneric("cleave"))
 }
--- R/rules.R
+++ R/rules.R
@@ -1,18 +1,3 @@
-## Copyright 2013 Sebastian Gibb
-## <mail@sebastiangibb.de>
-##
-## This is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## It is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## See <http://www.gnu.org/licenses/>
-
 ## cleavage rules are taken from:
 ## http://web.expasy.org/peptide_cutter/peptidecutter_enzymes.html
 rules <- c(
